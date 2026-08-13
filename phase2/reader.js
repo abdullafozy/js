1809940d1a68253bec38d0ced465e3c5ffc5d22f
abdullafozy <abdullakfozy@gmail.com>
2026-08-14 02:06:08 +0300

    Delete phase2 directory

--- phase2/reader.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const { parseCSV } = require("./parser");
-const csv = fs.readFileSync("./test.CSV", "utf8");
-
-const result = parseCSV(csv);
-
-console.log(JSON.stringify(result, null, 2));
